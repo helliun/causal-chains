71187fca968f1241b235887d3bb8fd0dcbd5af0f
helliun <65739931+helliun@users.noreply.github.com>
2023-02-09 14:42:28 -0500

    fixing imports

--- build/lib/causal_chains/__init__.py
+++
@@ -1,1 +0,0 @@
-name='causal_chains'
--- setup.py
+++ setup.py
@@ -5,15 +5,17 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="causal_chains",
-    version="1.0.1",
+    version="1.0.13",
     author="Henry Leonardi",
     author_email="leonardi.henry@gmail.com",
-    description="A package for extracting causal relationships",
+    description="A package for extracting causal chains from text",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/helliun/causal-chains",
-    packages=setuptools.find_packages(),
-    install_requires=['transformers','sentence-transformers','pydot','tqdm','torch','ipython'],
+    packages=["causal_chains"],#setuptools.find_packages(),
+    #py_modules = ["src/causal_chains"],
+    install_requires = ["hatchling","transformers","sentence-transformers","pydot","tqdm"]
+,
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
